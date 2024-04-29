47314130cfe0bdb7f81a88ef754d9669ac5aeaec
khadija <khadijaezzahidi5@gmail.com>
2024-04-29 02:06:54 +0200

    b1

---
+++ Backend/luxevuehotel/src/main/java/com/example/luxevuehotel/controller/RoomController.java
@@ -0,0 +1,83 @@
+package com.example.luxevuehotel.controller;
+
+import com.example.luxevuehotel.model.Room;
+import com.example.luxevuehotel.response.RoomResponse;
+import com.example.luxevuehotel.service.IRoomService;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("http://localhost:5173/")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rooms")
+public class RoomController {
+    private final IRoomService roomService;
+    @PostMapping("/add/new-room")
+    public ResponseEntity<RoomResponse> addNewRoom(
+            @RequestParam("photo") MultipartFile photo ,
+            @RequestParam("roomType")String roomType,
+            @RequestParam("roomPrice") BigDecimal roomPrice) throws SQLException, IOException {
+        Room savedRoom=roomService.addNewRoom(photo,roomType,roomPrice);
+        RoomResponse response=new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(),
+                savedRoom.getRoomPrice());
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes() {
+        return roomService.getAllRoomTypes();
+    }
+
+@CrossOrigin("http://localhost:5173/")
+    @GetMapping("/all-rooms")
+    public ResponseEntity<List<RoomResponse>> getAllRooms() throws SQLException {
+        List<Room> rooms = roomService.getAllRooms();
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for (Room room : rooms) {
+            byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
+            if (photoBytes != null && photoBytes.length > 0) {
+                String base64Photo = Base64.encodeBase64String(photoBytes);
+                RoomResponse roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(base64Photo);
+                roomResponses.add(roomResponse);
+            }
+        }
+        return ResponseEntity.ok(roomResponses);
+    }
+
+    @CrossOrigin("http://localhost:5175/")
+
+    @DeleteMapping("/delete/room/{roomId}")
+
+    public ResponseEntity<Void> deleteRoom(@PathVariable Long roomId){
+        roomService.deleteRoom(roomId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("/update/{roomId}")
+    public ResponseEntity<RoomResponse> updateRoom(@PathVariable Long roomId,
+                                                   @RequestParam(required = false)  String roomType,
+                                                   @RequestParam(required = false) BigDecimal roomPrice,
+                                                   @RequestParam(required = false) MultipartFile photo) throws SQLException, IOException {
+        byte[] photoBytes = photo != null && !photo.isEmpty() ?
+                photo.getBytes() : roomService.getRoomPhotoByRoomId(roomId);
+        Blob photoBlob = photoBytes != null && photoBytes.length > 0 ? new SerialBlob(photoBytes) : null;
+        Room theRoom = roomService.updateRoom(roomId, roomType, roomPrice, photoBytes);
+        theRoom.setPhoto(photoBlob);
+        RoomResponse roomResponse = getRoomResponse(theRoom);
+        return ResponseEntity.ok(roomResponse);
+    }
+}
