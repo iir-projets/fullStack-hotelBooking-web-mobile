f62b8611ef4b1f437bb357004d5fa38588673a34
HabibTablioua <habibtablioua1971@email.com>
2024-05-09 21:15:21 +0100

    comm-layout

---
+++ layout/MainHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1>
+					Welcome to <span className="hotel-color"> LuxeVue Hotel</span>
+				</h1>
+				<h4>Escape to Serenity and Luxury Living</h4>
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
